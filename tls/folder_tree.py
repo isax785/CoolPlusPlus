f613411e1696ef83d76d46ea07fde301a1d84135
i.stiaccini <i.stiaccini@frigel.com>
2025-04-22 16:35:24 +0200

    fixed folder tree generation

--- tls/folder_tree.py
+++ tls/folder_tree.py
@@ -7,28 +7,31 @@ def folder_mapping(path, out='', t=0, printout=False):
     - printout: bool, default=False, print the path/file
     """
     t_next = t + 1
-    for folder in os.walk(path):
-        s = "\t" * t + f"Folder: {folder[0]}"
+    for folder_path, subfolders, files in os.walk(path):
+        # Exclude folders in-place
+        subfolders[:] = [d for d in subfolders if d not in EXCLUDED_FOLDERS]
+
+        s = "\t" * t + f"Folder: {folder_path}"
         out += s + '\n'
-        if printout: print(s)
-        subfolders = folder[1]
-        for subfolder in subfolders:
-            subpath = os.path.join(path, subfolder)
-            if subfolder not in EXCLUDED_FOLDERS \
-                and all([ef not in subpath for ef in EXCLUDED_FOLDERS]):
-                # FIXME: some paths are not properly excluded
-                out += folder_mapping(subpath, 
-                               t=t_next, 
-                               printout=printout)
-        files = folder[2]
+        if printout:
+            print(s)
+
         for file in files:
             s = "\t" * t + f"- {file}"
             out += s + '\n'
-            if printout: print(s)
+            if printout:
+                print(s)
+
+        # Recursively call for remaining subfolders
+        for subfolder in subfolders:
+            subpath = os.path.join(folder_path, subfolder)
+            out += folder_mapping(subpath,
+                                    t=t_next,
+                                    printout=printout)
     return out
-    
+
 if __name__ == "__main__":
     folder_path = os.path.join(os.path.dirname(__file__), '..')
     out = folder_mapping(folder_path, printout=False)
-    with open("folder_tree.txt", 'w') as f: 
+    with open("folder_tree.txt", 'w') as f:
         f.write(out)
